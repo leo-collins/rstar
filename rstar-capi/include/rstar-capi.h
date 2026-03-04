7260ed70e25f3296b5e027e16251904f9c9f5516
Leo Collins <collins.leo@proton.me>
2026-03-04 12:27:57 +0000

    add bulk load and locate all at point

--- rstar-capi/include/rstar-capi.h
+++ rstar-capi/include/rstar-capi.h
@@ -7,8 +7,20 @@
 
 typedef struct RTreeH RTreeH;
 
+void rtree_bulk_load(struct RTreeH **tree,
+                     const double *mins,
+                     const double *maxs,
+                     const size_t *data,
+                     size_t n,
+                     uint32_t dim);
+
 void rtree_create(struct RTreeH **tree, uint32_t dim);
 
 void rtree_free(struct RTreeH *tree);
 
 void rtree_get_dimension(const struct RTreeH *tree, uint32_t *dim);
+
+void rtree_locate_all_at_point(const struct RTreeH *tree,
+                               const double *point,
+                               size_t **ids_out,
+                               size_t *nids_out);
--- rstar-capi/src/lib.rs
+++ rstar-capi/src/lib.rs
@@ -41,6 +41,13 @@ pub extern "C" fn rtree_free(
 }
 
 
+fn _rtree_get_dimension(tree: &RTreeDim) -> u32 {
+    match tree {
+        RTreeDim::D2(_) => 2,
+        RTreeDim::D3(_) => 3,
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn rtree_get_dimension(
     tree: *const RTreeH,
@@ -50,10 +57,74 @@ pub extern "C" fn rtree_get_dimension(
         return;
     }
     let rtree = unsafe { &*(tree as *const RTreeDim) };
-    let dimension = match rtree {
-        RTreeDim::D2(_) => 2,
-        RTreeDim::D3(_) => 3,
-    };
-    unsafe { *dim = dimension };
+    unsafe { *dim = _rtree_get_dimension(rtree) };
 }
 
+fn _rtree_bulk_load<const DIM: usize>(
+    mins: *const f64,
+    maxs: *const f64,
+    data: *const usize,
+    n: usize,
+) -> RTree<GeomWithData<Rectangle<[f64; DIM]>, usize>> {
+    let mins = unsafe { std::slice::from_raw_parts(mins, n * DIM) };
+    let maxs = unsafe { std::slice::from_raw_parts(maxs, n * DIM) };
+    let data = unsafe { std::slice::from_raw_parts(data, n) };
+
+    let objects = (0..n)
+        .map(|i| {
+            let min: [f64; DIM] = std::array::from_fn(|j| mins[i * DIM + j]);
+            let max: [f64; DIM] = std::array::from_fn(|j| maxs[i * DIM + j]);
+            GeomWithData::new(Rectangle::from_corners(min, max), data[i])
+        })
+        .collect();
+
+    RTree::bulk_load(objects)
+}
+
+#[no_mangle]
+pub extern "C" fn rtree_bulk_load(
+    tree: *mut *mut RTreeH,
+    mins: *const f64,
+    maxs: *const f64,
+    data: *const usize,
+    n: usize,
+    dim: u32,
+) {
+    if tree.is_null() || mins.is_null() || maxs.is_null() || data.is_null() {
+        return;
+    }
+
+    let rtree = match dim {
+        2 => RTreeDim::D2(_rtree_bulk_load::<2>(mins, maxs, data, n)),
+        3 => RTreeDim::D3(_rtree_bulk_load::<3>(mins, maxs, data, n)),
+        _ => return, // Invalid dimension
+    };
+
+    unsafe { *tree = Box::into_raw(Box::new(rtree)) as *mut RTreeH };
+}
+
+
+#[no_mangle]
+pub extern "C" fn rtree_locate_all_at_point(
+    tree: *const RTreeH,
+    point: *const f64,
+    ids_out: *mut *mut usize,
+    nids_out: *mut usize,
+) {
+    if tree.is_null() || point.is_null() || ids_out.is_null() || nids_out.is_null() {
+        return;
+    }
+    let rtree = unsafe { &*(tree as *const RTreeDim) };
+    let dim = _rtree_get_dimension(rtree) as usize;
+    let point = unsafe { std::slice::from_raw_parts(point, dim) };
+
+    let ids: Vec<usize> = match rtree {
+        RTreeDim::D2(tree) => tree.locate_all_at_point(point.try_into().unwrap()).map(|obj| obj.data).collect(),
+        RTreeDim::D3(tree) => tree.locate_all_at_point(point.try_into().unwrap()).map(|obj| obj.data).collect(),
+    };
+
+    unsafe {
+        *ids_out = ids.as_ptr() as *mut usize;
+        *nids_out = ids.len();
+    }
+}
